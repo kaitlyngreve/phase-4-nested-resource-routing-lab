b03d11d47ef03b3c93a90cc4341f1ebcfcf4daa8
Kaitlyn Greve <kgreve14@gmail.com>
2022-06-08 16:42:59 -0400

    commit tree a0bcd35ae3611d72660ec629e808b4572a5f1042

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :items, only: [:index] 
+
+
+  resources :users, only: [:index, :show] do
+    # we want to show all the items associated with the individual user
+    resources :items, only: [:index, :show, :create]
+  end
 end
